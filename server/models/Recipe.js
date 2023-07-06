77d457feaddc65e3cfaef067b5aa64bc334cdea1
kristiyan <kristiyan.tefov@icloud.com>
2023-07-06 18:59:23 +0100

    created resolvers starters

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -12,6 +12,11 @@ const recipeSchema = new Schema({
     image: {
         type: String,
     },
+    // we need to find what id will be returned form the fetch from the api this might change
+    recipeId: {
+        type: String,
+        required: true,
+    }
 
 });
 
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,3 +3,74 @@ const { User } = require("../models");
 const { AuthenticationError } = require("apollo-server-express");
 const { signToken } = require("../utils/auth")
 
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (!context.user) {
+        throw new AuthenticationError("You must be logged in");
+      } 
+      const user = await User.findById(context.user._id);
+
+      return user
+
+    }
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    
+    saveRecipe: async (parent, { recipeData }, context) => {
+    
+      if (!context.user) {
+        throw new AuthenticationError("You must be logged in");
+      }
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $addToSet: { savedRecipes: recipeData } },
+        { new: true }
+      );
+      return updatedUser;
+    },
+
+    
+    removeBook: async (parent, { recipeId }, context) => {
+      if (!context.user) {
+        throw new AuthenticationError("You must be logged in");
+      }
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $pull: { savedRecipes: { recipeId: recipeId } } },
+        { new: true }
+      );
+
+      return updatedUser
+    },
+
+  
+  },
+};
+
+module.exports = resolvers
