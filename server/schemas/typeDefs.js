b769ae778a879e44e8c6c9b8907ffee4a055d954
kristiyan <kristiyan.tefov@icloud.com>
2023-07-06 20:59:06 +0100

    typeDefs re-do mistakes typos and adding more options to user

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,37 +1,52 @@
 const {gql} = require('apollo-server-express')
 
-const  typeDefs = gql`
-  type Recipe{
-    _id: ID
-    uri:String
-    label:String
-    image: String
+const typeDefs = gql`
 
-  }
-
-  type User {
-    _id: ID
-    usename: String
-    email: String
-    password: String
-    
-  }
-
-  type Auth {
+type Auth {
     token: ID!
     user: User
-  }
+}
 
-  type Query {
-  me: User
-  }
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    recipeCount: Int
+    savedRecipes: [Recipe]
+}
 
-  type Mutation {
-    addUser(username: String!, email: String!, password: String!): Auth
-    login(email String!, password: String!): Auth
-    saveRecipe(recipeData) Recipe
-    removeBook( recipeId:ID) Recipe
-  }
-`;
+type Recipe {
+    _id: ID!
+    recipeId: ID!
+    label: String!
+    uri: [String]
+    image: String
+    
 
+}
+input RecipeInput {
+  
+    recipeId: ID!
+    label: String!
+    uri: [String]
+    image: String
+}
+
+input UserInput {
+  username: String!
+    email: String!
+    password: String!
+}
+
+type Query {
+    me: User!
+}
+
+type Mutation {
+    addUser(userData: UserInput!): Auth
+    login(email: String!, password: String!): Auth
+    saveRecipe(recipeData: BookInput!): User
+    removeRecipe(recipeId: String!): User
+}
+`
 module.exports = typeDefs;
