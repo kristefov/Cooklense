8befd00c950577dd022bfdbbafb535e2fdb86fcf
Hodan Ibrahim <hodanibrahim@Hodans-MacBook-Air.local>
2023-07-06 19:40:57 +0100

    created typeDefs file

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,2 +1,36 @@
 const {gql} = require('apollo-server-express')
 
+const  typeDefs = gql`
+  type Recipe{
+    _id: ID
+    uri:String
+    label:String
+    image: String
+
+  }
+  type User {
+    _id: ID
+    usename: String
+    email: String
+    password: String
+    
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query{
+  Recipe( Recipe)
+  user: User
+  }
+
+  type Mutation {
+    login(username: String!, password: String!): Auth
+    signup(userData: UserInput!): Auth
+
+  }
+`;
+
+module.exports = typeDefs;
