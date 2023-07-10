4d99a427e646e4803619ffd938fc7f13a4f71e5c
Abstynent <lukjur94@gmail.com>
2023-07-10 20:40:02 +0100

    searchByID query added to recipeSearch function that is fetching data from API

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,11 +5,19 @@ export const recipeSearch = async (query, value) => {
     );
     const data = await response.json();
     return data;
-  }
+  };
 
   if (query === "searchIngredient") {
     const response = await fetch(
       `https://www.themealdb.com/api/json/v1/1/filter.php?i=${value}`
+    );
+    const data = await response.json();
+    return data;
+  };
+
+  if(query === "searchById") {
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${value}`
     );
     const data = await response.json();
     return data;
