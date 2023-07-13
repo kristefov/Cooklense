e5484372645904cae65910609a4ba712d696e796
Abstynent <lukjur94@gmail.com>
2023-07-13 21:20:10 +0100

    added 2 new dropdown list to allow user search by cusine or category, functions added to support that logic, useEffect to fetch from API updated list of categories and countries

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { getCategories, getCountries } from "../../utils/API";
+
 import {
   Container,
   Col,
@@ -11,19 +13,53 @@ import {
   Alert,
 } from "react-bootstrap";
 
-const options = [
+const mainSearchOptions = [
   { value: "name", label: "Search by Name" },
   { value: "ingredient", label: "Search by Ingredient" },
 ];
 
 const Home = () => {
-
   const [searchInput, setSearchInput] = useState("");
-  const [dropdownValue, setDropdownValue] = useState(options[0].value);
+  const [dropdownValue, setDropdownValue] = useState(
+    mainSearchOptions[0].value
+  );
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [allCountries, setAllCountries] = useState([]);
+  const [allCategories, setAllCategories] = useState([]);
+  const navigate = useNavigate();
   const [showAlert, setShowAlert] = useState(false);
+
+  useEffect(() => {
+    getCountries().then((countries) => {
+      setAllCountries(countries);
+    });
+  }, []);
+
+  useEffect(() => {
+    getCategories().then((categories) => {
+      setAllCategories(categories);
+    });
+  }, []);
 
   const handleAlertDismiss = () => {
     setShowAlert(false);
+  };
+
+  const handleCountryChange = (e) => {
+    const selectedCountry = e.target.value;
+    setSelectedCountry(selectedCountry);
+    if (selectedCountry !== "") {
+      navigate(`/search/cusine/${selectedCountry}`);
+    }
+  };
+
+  const handleCategoryChange = (e) => {
+    const selectedCategory = e.target.value;
+    setSelectedCategory(selectedCategory);
+    if (selectedCategory !== "") {
+      navigate(`/search/category/${selectedCategory}`);
+    }
   };
 
   return (
@@ -51,7 +87,7 @@ const Home = () => {
                   value={dropdownValue}
                   onChange={(e) => setDropdownValue(e.target.value)}
                 >
-                  {options.map((option) => (
+                  {mainSearchOptions.map((option) => (
                     <option key={option.value} value={option.value}>
                       {option.label}
                     </option>
@@ -78,6 +114,29 @@ const Home = () => {
             </Alert>
           )}
         </Form>
+        <Row>
+          <Col xs={12} md={8}>
+          <div>
+              <select value={selectedCountry} onChange={handleCountryChange}>
+                <option value="">Select a cusine</option>
+                {allCountries.map((country, index) => (
+                  <option key={index} value={country.strArea}>
+                    {country.strArea}
+                  </option>
+                ))}
+              </select>
+
+              <select value={selectedCategory} onChange={handleCategoryChange}>
+                <option value="">Select a category</option>
+                {allCategories.map((category, index) => (
+                  <option key={index} value={category.strCategory}>
+                    {category.strCategory}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </Col>
+        </Row>
       </Container>
     </>
   );
