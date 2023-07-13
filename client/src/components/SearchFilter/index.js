9073cdfb668e4218e14fb1a962aaad3ec420068f
Abstynent <lukjur94@gmail.com>
2023-07-13 17:24:46 +0100

    component created to display search filters on SearchResults page

---
+++ client/src/components/SearchFilter/index.js
@@ -0,0 +1,91 @@
+import React from "react";
+import { Container, Form } from "react-bootstrap";
+
+const options = {
+  general: {
+    sugar: "Sugar",
+    eggs: "Eggs",
+    flour: "Flour",
+    nuts: "Nuts",
+  },
+  dairy: {
+    milk: "Milk",
+    butter: "Butter",
+    cream: "Cream",
+    parmesan: "Parmesan",
+    gruyere: "Gruyere",
+    cheddar: "Cheddar",
+  },
+  meatFish: {
+    pork: "Pork",
+    chicken: "Chicken",
+    prawn: "Prawn",
+    duck: "Duck",
+    prosciutto: "Prosciutto",
+    salmon: "Salmon",
+  },
+  vegetables: {
+    onion: "Onion",
+    broccoli: "Broccoli",
+    pepper: "Pepper",
+    tomatoes: "Tomaotes",
+    ginger: "Ginger",
+    garlic: "Garlic",
+    potatoes: "Potatoes",
+    leek: "Leek",
+  },
+};
+
+const SearchFilter = ({ onChange, onClick }) => {
+  return (
+    <Container>
+      <Form onChange={onChange}>
+        <Form.Label>This Product Contains:</Form.Label>
+        {Object.keys(options.general).map((key) => (
+          <Form.Check
+            key={key}
+            type="checkbox"
+            name={key}
+            label={options.general[key]}
+            onClick={onClick}
+          />
+        ))}
+
+        <Form.Label>Dairy products</Form.Label>
+        {Object.keys(options.dairy).map((key) => (
+          <Form.Check
+            key={key}
+            type="checkbox"
+            name={key}
+            label={options.dairy[key]}
+            onClick={onClick}
+          />
+        ))}
+
+        <Form.Label>Meat & Fish</Form.Label>
+        {Object.keys(options.meatFish).map((key) => (
+          <Form.Check
+            key={key}
+            type="checkbox"
+            name={key}
+            label={options.meatFish[key]}
+            onClick={onClick}
+          />
+        ))}
+
+        <Form.Label>Vegetables</Form.Label>
+        {Object.keys(options.vegetables).map((key) => (
+          <Form.Check
+            key={key}
+            type="checkbox"
+            name={key}
+            label={options.vegetables[key]}
+            onClick={onClick}
+          />
+        ))}
+      </Form>
+    </Container>
+  );
+};
+
+export default SearchFilter;
