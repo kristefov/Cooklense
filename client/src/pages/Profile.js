0532c2c4ed4ef353db16d9ca9a6ef3b97b1e0b41
dExIT <dexit@dyc.lv>
2023-07-13 22:48:44 +0100

    added base profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+
+import { Navbar, Nav, Container, Col, Row, Image } from 'react-bootstrap';
 
 function Profile() {
   return (
-    <div>Profile</div>
-  )
+<>
+<Container className="p-3 container-fluid d-flex flex-column align-center">
+      <h1>Profile</h1>
+      <div class="card">
+        <Image class="card-img-top" src="holder.js/100x180/?text=Image cap" alt="Card image cap"></Image>
+        <div class="card-body">
+          <h4 class="card-title">Title</h4>
+          <p class="card-text">Text</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Item 1</li>
+          <li class="list-group-item">Item 2</li>
+          <li class="list-group-item">Item 3</li>
+        </ul>
+      </div>
+     
+    </Container>
+    </>
+  );
 }
 
 export default Profile
