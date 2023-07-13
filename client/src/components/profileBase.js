3fd6a456f70dfbfd13208e04865bdba45b5dcfee
dExIT <dexit@dyc.lv>
2023-07-13 20:51:04 +0100

    testing lukasz code

---
+++ client/src/components/profileBase.js
@@ -0,0 +1,15 @@
+// NAVBAR
+import React from 'react';
+
+import { Navbar, Nav, Container, Col, Row, Image } from 'react-bootstrap';
+
+const ProfileBase = () => {
+    
+
+    return (
+        <>
+        
+        </>
+    );
+}
+export default ProfileBase
