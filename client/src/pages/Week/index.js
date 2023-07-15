a625ccf1c85bd84d74def344b31512f3bd8515f8
Abstynent <lukjur94@gmail.com>
2023-07-15 15:27:29 +0100

    handleRemoveMeal function in progress

--- client/src/pages/Week/index.js
+++ client/src/pages/Week/index.js
@@ -5,49 +5,66 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight, faTimes } from "@fortawesome/free-solid-svg-icons";
 import "./style.css";
 import { GET_ME } from "../../utils/queries";
-import { useQuery } from "@apollo/client";
+import { REMOVE_MEAL_FROM_WEEKPLAN } from "../../utils/mutations";
+import { useQuery, useMutation } from "@apollo/client";
 
 const WeekPlan = () => {
   const weekdays = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
   const { data } = useQuery(GET_ME);
+  const [removeMealFromWeekPlan] = useMutation(REMOVE_MEAL_FROM_WEEKPLAN);
   const [savedWeekPlan, setSavedWeekPlan] = useState([]);
 
   useEffect(() => {
     if (data && data.me && data.me.weekPlan) {
       setSavedWeekPlan(data.me.weekPlan);
+      console.log(savedWeekPlan)
     }
   }, [data]);
 
+  const handleRemoveMeal = async (idMeal) => {
+    console.log(data)
+    try {
+      await removeMealFromWeekPlan({
+        variables: {
+          idMeal: idMeal,
+        },
+      });
 
-  const handleRemoveMeal = (mealId) => {
-    console.log(`Removing meal with id ${mealId}`);
+      const updatedWeekPlan = savedWeekPlan.filter(
+        (meal) => meal.idMeal !== idMeal
+      );
+      setSavedWeekPlan(updatedWeekPlan);
+    } catch (error) {
+      console.log("Error removing meal:", error);
+    }
   };
 
   const mealItems = (meals) =>
-  meals.map((meal, index) => (
-    <div className="meal-item" key={index}>
-      <Link
-        to={`/recipe/${meal.idMeal}`}
-        style={{ textDecoration: "none", color: "black" }}
-        className="meal-link"
-      >
-        <p className="mt-1">{meal.strMeal}</p>
-      </Link>
-
-      <div className="meal-item-buttons">
-        <Button
-          variant="danger"
-          onClick={() => handleRemoveMeal(meal.idMeal)}
+    meals.map((meal, index) => (
+      <div className="meal-item" key={index}>
+        <Link
+          to={`/recipe/${meal.idMeal}`}
+          style={{ textDecoration: "none", color: "black" }}
+          className="meal-link"
         >
-          <FontAwesomeIcon icon={faTimes} />
-        </Button>
-      </div>
-    </div>
-  ));
+          <p className="mt-1">{meal.strMeal}</p>
+        </Link>
 
+        <div className="meal-item-buttons">
+          <Button
+            variant="danger"
+            onClick={() => handleRemoveMeal(meal.idMeal)}
+          >
+            <FontAwesomeIcon icon={faTimes} />
+          </Button>
+        </div>
+      </div>
+    ));
 
   const columns = weekdays.map((weekday, index) => {
-    const mealsByDay = savedWeekPlan.filter((meal) => meal.day === weekday).flatMap((meal) => meal.recipeData);
+    const mealsByDay = savedWeekPlan
+      .filter((meal) => meal.day === weekday)
+      .flatMap((meal) => meal.recipeData);
     return (
       <div className="column" key={index}>
         <div className="column-header">{weekday}</div>
