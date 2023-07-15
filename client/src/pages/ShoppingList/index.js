1167265c37144376647517a009c86f1c30cd0c27
Abstynent <lukjur94@gmail.com>
2023-07-16 00:29:46 +0100

    logic added to remove ingredient from the list

--- client/src/pages/ShoppingList/index.js
+++ client/src/pages/ShoppingList/index.js
@@ -2,18 +2,27 @@ import React from "react";
 import { Button, Container, ListGroup } from "react-bootstrap";
 import { useMutation, useQuery } from "@apollo/client";
 import { GET_ME } from "../../utils/queries";
-// import { REMOVE_INGREDIENT } from "../../utils/mutations";
+import { REMOVE_INGREDIENT_FROM_SHOPPING_LIST } from "../../utils/mutations";
 
 const ShoppingList = () => {
   const { loading, error, data, refetch } = useQuery(GET_ME);
   const user = data?.me;
   const savedIngredients = user?.shoppingList || [];
-  console.log(savedIngredients);
 
-//   const [removeIngredient] = useMutation(REMOVE_INGREDIENT);
+  const [removeIngredient] = useMutation(REMOVE_INGREDIENT_FROM_SHOPPING_LIST);
 
-  const handleRemoveIngredient = async (ingredientId) => {
-    return;
+  const handleRemoveIngredient = async (ingredient) => {
+    console.log(ingredient)
+    try {
+      await removeIngredient({
+        variables: {
+          ingredient,
+        },
+      });
+      refetch();
+    } catch (error) {
+      console.error("Error removing ingredient:", error);
+    }
   };
 
   if (loading) {
@@ -28,21 +37,25 @@ const ShoppingList = () => {
     <Container className="d-flex justify-content-center">
       <div className="w-50">
         <h3 className="text-center">Saved Recipes</h3>
-        <ListGroup>
-          {savedIngredients.map((ingredient, index) => (
-            <ListGroup.Item key={index}>
-              <Button
-                variant="link"
-                size="sm"
-                className="text-danger"
-                onClick={() => handleRemoveIngredient(ingredient)}
-              >
-                X
-              </Button>
-              {ingredient}
-            </ListGroup.Item>
-          ))}
-        </ListGroup>
+        {savedIngredients.length === 0 ? (
+          <p className="text-center">Your shopping list is empty</p>
+        ) : (
+          <ListGroup>
+            {savedIngredients.map((ingredient, index) => (
+              <ListGroup.Item key={index}>
+                <Button
+                  variant="link"
+                  size="sm"
+                  className="text-danger"
+                  onClick={() => handleRemoveIngredient(ingredient)}
+                >
+                  X
+                </Button>
+                {ingredient}
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
       </div>
     </Container>
   );
