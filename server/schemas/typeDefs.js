ee1246c13725eadd608fa3e802d864b0b69df8bc
Abstynent <lukjur94@gmail.com>
2023-07-15 14:47:45 +0100

    added weekplayday type that is part of user to store weekplan, added mutation addToWeekPlan

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,6 +4,7 @@ const typeDefs = gql`
     token: ID!
     user: User
   }
+
   type User {
     _id: ID!
     firstName: String!
@@ -13,18 +14,26 @@ const typeDefs = gql`
     recipeCount: Int
     avatar: String
     savedRecipes: [Recipe]
+    weekPlan: [WeekPlanDay]
   }
+
+  type WeekPlanDay {
+    day: String!
+    recipeData: [Recipe!]!
+  }
+
   type Recipe {
-    _id: ID!
     idMeal: String
     strMeal: String
     strMealThumb: String
   }
+
   input RecipeInput {
     idMeal: String
     strMeal: String
     strMealThumb: String
   }
+
   input UserInput {
     firstName: String
     lastName: String
@@ -33,10 +42,10 @@ const typeDefs = gql`
     password: String
     avatar: String
   }
+
   type Query {
     me: User!
   }
-
 
   type Mutation {
     addUser(userData: UserInput!): Auth
@@ -44,6 +53,7 @@ const typeDefs = gql`
     saveRecipe(recipeData: RecipeInput!): User
     removeRecipe(recipeId: String!): User
     updateUser(userData: UserInput!): User
+    addToWeekPlan(day: String!, recipeData: RecipeInput!): User
   }
 `;
 module.exports = typeDefs;
