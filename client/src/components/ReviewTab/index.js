273a600634a68232a307950bfc2fa0c9d61a2109
Abstynent <lukjur94@gmail.com>
2023-07-16 22:23:24 +0100

    added functionality to handleSubmit to createReview and save it in the database

--- client/src/components/ReviewTab/index.js
+++ client/src/components/ReviewTab/index.js
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import StarRatingComponent from "react-star-rating-component";
 import { Form, Button } from "react-bootstrap";
+import { useMutation } from "@apollo/client";
+import { CREATE_REVIEW } from "../../utils/mutations";
+import { useSelector } from "react-redux/es/hooks/useSelector";
 
-const ReviewTab = () => {
+const ReviewTab = ({ idMeal }) => {
   const [rating, setRating] = useState(5);
   const [title, setTitle] = useState("");
   const [review, setReview] = useState("");
   const [formError, setFormError] = useState(false);
+  const [createReview] = useMutation(CREATE_REVIEW);
+  const { user } = useSelector((state) => state.auth);
+  const { username } = user;
 
   const onStarClick = (nextRating) => {
     setRating(nextRating);
@@ -22,21 +28,31 @@ const ReviewTab = () => {
     setFormError(false);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (
-      rating === 0 ||
-      title.trim().length < 5 ||
-      review.trim().length < 5
-    ) {
+    if (rating === 0 || title.trim().length < 5 || review.trim().length < 5) {
       setFormError(true);
       return;
     }
+    const reviewData = {
+      idMeal: idMeal,
+      username: username,
+      rating: rating,
+      title: title,
+      comment: review
+    };
 
-    console.log(rating);
-    console.log(title);
-    console.log(review);
+    await createReview({variables: { reviewData}})
+    .then((response) => {
+      console.log('Review created.');
+    })
+    .catch((error) => {
+      console.error("Error creating review:", error);
+    })
+    setTitle("");
+    setReview("");
+    setRating(5);
   };
 
   return (
@@ -57,7 +73,9 @@ const ReviewTab = () => {
           placeholder="Title"
           value={title}
           onChange={handleTitleChange}
-          isInvalid={formError && (title.trim().length < 5 || title.trim() === "")}
+          isInvalid={
+            formError && (title.trim().length < 5 || title.trim() === "")
+          }
         />
         <Form.Control
           as="textarea"
@@ -65,7 +83,9 @@ const ReviewTab = () => {
           rows={3}
           value={review}
           onChange={handleReviewChange}
-          isInvalid={formError && (review.trim().length < 5 || review.trim() === "")}
+          isInvalid={
+            formError && (review.trim().length < 5 || review.trim() === "")
+          }
         />
         {formError && (
           <Form.Control.Feedback type="invalid">
@@ -73,11 +93,13 @@ const ReviewTab = () => {
           </Form.Control.Feedback>
         )}
       </Form.Group>
-      <div style={{ display: "flex", justifyContent: "center", marginTop: "1rem" }}>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </div>
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "1rem" }}
+      >
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </div>
     </Form>
   );
 };
